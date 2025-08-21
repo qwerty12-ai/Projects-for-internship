a799aa474903d3c93a48297199dc27ff332ec234
qwerty12-ai <sabeehmohd79@gmail.com>
2025-08-21 04:43:30 -0700

    Prepare for Heroku deployment

--- website generator/app.py
+++ website generator/app.py
@@ -96,3 +96,8 @@ def history():
 def generated():
     # Automatically generate html website
     return send_from_directory(os.getcwd(), 'templates/generated.html')
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
